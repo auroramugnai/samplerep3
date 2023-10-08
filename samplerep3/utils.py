89c6fb8ffb42e19beddc8937ebd1c6626ba94abc
auroramugnai <a.mugnai2@studenti.unipi.it>
2023-10-08 11:34:53 +0200

    Directories import.

---
+++ samplerep3/utils.py
@@ -0,0 +1,17 @@
+"""General utilities.
+"""
+
+
+def square(value):
+    '''Return the square of a given number.
+
+    Arguments
+    ------
+    value : number
+        The input
+    
+    Return
+    -----
+        The square.
+    '''
+    return value**2.
---
+++ tests/test_utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+from samplerep.utils import square
+
+def test_square():
+	"""Unit test for the square() function.
+	"""
+	assert square(2.) == 4.# the reason this works is some magic of pytest (non dovresti mai eguagliare floating point)
+	assert square(-2.) == 4.
+	assert square(0.) == 0.
+	assert square(2) == 4.
+	assert square(-2) == 4.
+	assert square(0) == 0
+	# assert square(np.ones(100)) == np.ones(100)
+	assert np.allclose(square(np.ones(100)), np.ones(100))# close enough given errore di arrotondam della macchina
